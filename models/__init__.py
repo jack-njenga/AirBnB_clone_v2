cf5611268787faeb851059430175a2280efa85cb
jack-njenga <jacknjenga99@gmail.com>
2023-09-11 18:15:59 +0300

    db type

--- models/__init__.py
+++ models/__init__.py
@@ -7,9 +7,9 @@ engine_type = getenv("ST")
 if engine_type == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    print("\t\t--DB ENGINE--")
+    # print("\t\t--DB ENGINE--")
 else:
-    print("\t\t--FILE ENGINE--")
+    # print("\t\t--FILE ENGINE--")
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
