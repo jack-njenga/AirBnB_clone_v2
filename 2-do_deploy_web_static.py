2ea33cea4529230399ddc031d7f273af8838b729
jack-njenga <jacknjenga99@gmail.com>
2023-09-22 16:18:10 +0300

    Hello Flask!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,8 +4,23 @@ Distributes an archive to your web servers, using the function do_deploy
 """
 from fabric.api import *
 import os
+from datetime import datetime
 
+fmt = "%Y%m%d%H%M%S"
 env.hosts = ["100.26.161.31", "18.204.7.85"]
+
+def do_pack():
+    """
+    Generates a ".tgz" file
+    """
+    local("mkdir -p versions")
+    dt = datetime.strftime(datetime.now(), fmt)
+    path = (f"versions/web_static_{dt}.tgz")
+    result = local(f"tar -cvzf {path} web_static")
+
+    if result.failed:
+        return None
+    return path
 
 
 def do_deploy(archive_path):
@@ -13,14 +28,14 @@ def do_deploy(archive_path):
     do_deploy method
     """
     if not os.path.exists(archive_path):
-        return 0
+        return False
     else:
         put(archive_path, "/tmp/")
         name = archive_path.split("/")[-1]
         no_fmt = name.split(".")[0]
         path = "/data/web_static/releases/"
 
-        sudo("mkdir -p {path}{no_fmt}/")
+        sudo(f"mkdir -p {path}{no_fmt}/")
         sudo(f"tar -xvf /tmp/{name} -C {path}{no_fmt}/")
         sudo(f"rm /tmp/{name}")
         sudo(f"mv {path}{no_fmt}/web_static/* {path}{no_fmt}/")
@@ -29,4 +44,4 @@ def do_deploy(archive_path):
         sudo(f"ln -s {path}{no_fmt} /data/web_static/current")
 
         print("New version deployed!")
-        return 1
+        return True
--- web_static/README.md
+++ web_static/README.md
@@ -1,1 +1,6 @@
-This directory implements webpage design.
+## AirBnB Clone-Web Static
+### Frontend Part
+
+- . - Contains all the actual html files.
+- /styles - Contains all the css files.
+- /images - Contains all the images and logos.
